932842b05ac5211ed7ad53a87deea5cf732da6e9
Will Mendes <willmendesneto@gmail.com>
2019-04-08 22:13:48 +1000

    fix: adding public api entrypoints for new directives

--- projects/ngx-feature-toggle/src/public_api.ts
+++ projects/ngx-feature-toggle/src/public_api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-feature-toggle
  */
 
-export * from './lib/ngx-feature-toggle.component';
+export * from './lib/ngx-feature-toggle.directive';
+export * from './lib/ngx-feature-toggle-when-disabled.directive';
 export * from './lib/ngx-feature-toggle-provider.component';
 export * from './lib/ngx-feature-toggle.module';
