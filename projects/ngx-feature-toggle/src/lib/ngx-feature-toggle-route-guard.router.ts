19ad821f87e9cded3533d65b8919439bbb5afe43
Brandon Smith <UA32493@DOW.COM>
2023-02-28 11:04:13 -0500

    fix(routeGuard): Allow non-null redirect
     - When route guard redirects, it will now redirect for ANY non-null path
       - This allows using '' for path, a common pattern for home pages
     - Added tests to assert behavior
    
    Closes #268

--- projects/ngx-feature-toggle/src/lib/ngx-feature-toggle-route-guard.router.ts
+++ projects/ngx-feature-toggle/src/lib/ngx-feature-toggle-route-guard.router.ts
@@ -29,7 +29,7 @@ export class NgxFeatureToggleRouteGuard implements CanActivateChild, CanLoad, Ca
       toggle[0] === '!' ? !isOn(toggle.replace('!', '')) : isOn(toggle),
     );
 
-    if (!hasAllTogglesOn && route.data.redirectTo) {
+    if (!hasAllTogglesOn && route.data.redirectTo != null) {
       this.router.navigate([].concat(route.data.redirectTo));
     }
 
